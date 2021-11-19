efbffe840ef9f4126845e10650c1dfe962400b3c
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-11-19 10:36:24 +0530

    Example

---
+++ EXAMPLE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 34.5;
+            object iBoxed = i;
+            int? jNullable = 42;
+            if (iBoxed is double a && jNullable is int b)
+            {
+                Console.WriteLine(a + b);  // output 76
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid datatype");
+                Console.ReadKey();
+            }
+        }
+    }
+}
