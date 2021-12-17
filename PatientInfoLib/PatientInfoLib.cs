871b328eb37a6b45868ac65102340c3f4830af88
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-12-17 12:45:17 +0000

    PatientInfoLibDll

---
+++ PatientInfoLib/PatientInfoLib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PatientInfoLib
+{
+    public class PatientInfoLib
+    {
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public double BmiValue { get; set; }
+        public double Result { get; set; }
+
+
+
+
+    }
+}
