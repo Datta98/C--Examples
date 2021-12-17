013c44f799caa4be342296783fab0d2c622461b7
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-12-17 12:46:09 +0000

    FileReaderLIb

---
+++ FileReaderLib/FileReaderType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FileReaderLib
+{
+    public class FileReaderType
+    {
+
+         public List<string> AllLineReader( string Path)
+        {
+           
+           System.IO.StreamReader reader = new System.IO.StreamReader (string path);
+            List<string> lines = new List<string>();
+            while(! reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                line.Add(line);
+            }
+            reader.Close();
+            return lines;
+           
+
+        }
+
+
+
+
+    }
+}
