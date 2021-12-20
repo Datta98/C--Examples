4411a3b2eb02cdc0a50a2b79a1decb4c4d784136
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-12-20 12:34:14 +0000

    StringValidatorApplication

---
+++ StringValidatorApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringValidatorApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data1 = null;
+            string data2 = "mysore";
+            string data3 = "Excel";
+            StringValidator nullorempty = new StringValidator( new IsNullOrEmpty());
+            nullorempty.IsValidString(data1);
+
+            StringValidator lengthcheck = new StringValidator(new LengthCheck());
+            lengthcheck.IsValidString(data2);
+
+            StringValidator startwith = new StringValidator(new StartWithCheck());
+            startwith.IsValidString(data3);
+           
+        }
+    }
+  
+}
